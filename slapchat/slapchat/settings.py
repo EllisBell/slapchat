35c1b714490cad05aee8eb3153ccc8683c30f747
EllisBell <tee.vee.zed@gmail.com>
2019-03-10 16:52:31 +0000

    adding heroku to allowed hosts fixed

--- slapchat/slapchat/settings.py
+++ slapchat/slapchat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SLAP_SECRET', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1'. '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
